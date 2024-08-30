99ad3ae8fd60ef0bdb2f7efa64e30a13932cc099
Vasyl Boliuk <vbboliuk@gmail.com>
2024-08-30 13:11:17 +0300

    feat: [BE-9] Update main to run endpoints

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+from fastapi import FastAPI
 
+from src.services.routers.version import router as version_router
 
-print(greet("World"))
+app = FastAPI()
+
+# Include the version router
+app.include_router(version_router, prefix="/api")
+
+if __name__ == "__main__":
+    import uvicorn
+    from src.core import constants
+
+    uvicorn.run(app, host=constants.DEFAULT_HOST, port=constants.DEFAULT_PORT)
