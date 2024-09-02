023ed505f1754f5ecc3bdb6680c0e320c888e576
Vasyl Boliuk <vbboliuk@gmail.com>
2024-09-02 18:47:27 +0300

    feat: [BE-9] fix version endpoint with pyndentic model

--- src/services/routers/version.py
+++ src/services/routers/version.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 from src.core.__version__ import VERSION
+from src.services.models.version import Version
 
 router = APIRouter()
 
 
-@router.get("/version", response_model=dict)
+@router.get("/version", response_model=Version)
 async def get_version():
     """
     Returns the current version of the API.
     """
-    return {"version": VERSION}
+    return Version(version=VERSION)
