10bccf1f12df883aa7c554b3844c2f42d0f40b1c
Vasyl Boliuk <vbboliuk@gmail.com>
2024-09-27 12:43:04 +0300

    feat: Add tests commands

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# Run any commands before the app starts, like database migrations
-# Example: poetry run alembic upgrade head
-
 # Start the application
-exec "find ~/repos/appointment-scheduler"
+echo "Start Application"
+find ~/app/appointment-scheduler
+exec "find ~/app/appointment-scheduler"
 exec "$@"
 exec "uvicorn src.main:app --reload"
